d72cf966c68835c384097ef24ff89b7f15323b08
Riolku <keenan@thegugelers.ca>
2020-05-30 11:24:13 -0400

    add port options in main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 from pharmacy import app
+
+from sys import argv
+
+if __name__ == "__main__":
+  port = 4010
+  
+  for i in range(len(argv)):
+    if argv[i].startswith("--port="):
+      port = int(argv[i][len("--port="):])
+  
+  app.run(host = "0.0.0.0", port = port)
